af6a91ed929aa54295f20c6fcf6cafbe4f10f2ac
lmislm <wszdlm@gmail.com>
2019-04-24 16:26:13 +0800

    完善目录结构

---
+++ src/lib/i18nConfig.ts
@@ -0,0 +1,9 @@
+const config = {
+  autoZhKey: true,
+  exclude: [],
+  autokeyPrefix: false, // true则以文件名称加_为键名
+  findPath: 'tests',
+  keyFileName: 'zh_pro',
+  targetDir: 'i18n-messages', // 新建文件名
+};
+export { config };
